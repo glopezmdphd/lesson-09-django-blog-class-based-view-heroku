695153dc043d800549da3f144cb5f7060582d9f3
George A Lopez MD PhD <glopezmdphd@gmail.com>
2024-07-10 10:28:51 -0700

    Temporarily simplify static files configuration by removing STATICFILES_DIRS

--- mysite/settings.py
+++ mysite/settings.py
@@ -1,8 +1,6 @@
 import os
 from pathlib import Path
 
-# Build paths inside the project like this: BASE_DIR / "subdir"
-BASE_DIR = Path(__file__).resolve().parent.parent
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/2.1/howto/deployment/checklist/
@@ -107,11 +105,14 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.1/howto/static-files/
 
+# Build paths inside the project like this: BASE_DIR / "subdir"
+BASE_DIR = Path(__file__).resolve().parent.parent
+
 STATIC_URL = "/static/"
 
-STATICFILES_DIRS = [
-    BASE_DIR / "static",
-]
+#STATICFILES_DIRS = [
+#    BASE_DIR / "static",
+#]
 
 STATIC_ROOT = BASE_DIR / "staticfiles"
 
